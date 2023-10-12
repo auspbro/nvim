1c9c11923f73ae94387f343e77ac314b1912509c
Alan <auspbro@gmail.com>
2023-10-12 16:11:06 +0800

    Update colorscheme.lua

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-	"theniceboy/nvim-deus",
+	-- "theniceboy/nvim-deus",
+	-- "auspbro/nvim-deus",
+	"ellisonleao/gruvbox.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme deus]])
+		-- vim.cmd([[colorscheme deus]])
+		vim.cmd([[colorscheme gruvbox]])
 	end,
 }
