f56201e23e5395da77f865946427029e84510127
Alan <auspbro@gmail.com>
2023-10-12 16:37:14 +0800

    Update colorscheme.lua

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -5,6 +5,12 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require("gruvbox").setup({
+			contrast = "hard",
+			overrides = { 
+				SignColumn = { bg = "#1d2021" }
+			},
+		})
 		-- vim.cmd([[colorscheme deus]])
 		vim.cmd([[colorscheme gruvbox]])
 	end,
